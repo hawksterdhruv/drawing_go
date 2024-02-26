70055e8c945d66631c2e23c67a77e89935133e21
dhruv <hawksterdhruv@gmail.com>
2024-02-25 21:00:58 -0500

    adding levy flight algo

--- walker/strategy.go
+++ walker/strategy.go
@@ -1,9 +1,30 @@
 package walker
 
-import "math/rand"
+import (
+	"math"
+	"math/rand"
+
+	"github.com/gopxl/pixel/v2"
+)
 
 type WalkingStrategy interface {
 	nextStep(*Walker)
+}
+
+type LevyFlightWalkingStrategy struct {
+}
+
+func (l *LevyFlightWalkingStrategy) nextStep(walker *Walker) {
+	chosen_angle := 2 * math.Pi * rand.Float64()
+	prob := 100 * rand.Float32() // Scaling to 100%
+	var chosen_direction pixel.Vec
+	step_size := 2.0
+	if prob < 1 { // 1% chance
+		step_size = 25 + float64(rand.Intn(25))
+	}
+
+	chosen_direction = pixel.Unit(chosen_angle).Scaled(step_size)
+	walker.CurrentPos = walker.CurrentPos.Add(chosen_direction)
 }
 
 type CardinalWalkingStrategy struct {
@@ -13,20 +34,20 @@ func (l *CardinalWalkingStrategy) nextStep(walker *Walker) {
 	chosen_direction := rand.Intn(4)
 	switch chosen_direction {
 	case 0: // UP
-		if walker.currentPos.Y < walker.height-1 {
-			walker.currentPos.Y += 1
+		if walker.CurrentPos.Y < walker.height-1 {
+			walker.CurrentPos.Y += 1
 		}
 	case 1: // RIGHT
-		if walker.currentPos.X < walker.width-1 {
-			walker.currentPos.X += 1
+		if walker.CurrentPos.X < walker.width-1 {
+			walker.CurrentPos.X += 1
 		}
 	case 2: // DOWN
-		if walker.currentPos.Y > 0 {
-			walker.currentPos.Y -= 1
+		if walker.CurrentPos.Y > 0 {
+			walker.CurrentPos.Y -= 1
 		}
 	case 3: // LEFT
-		if walker.currentPos.X > 0 {
-			walker.currentPos.X -= 1
+		if walker.CurrentPos.X > 0 {
+			walker.CurrentPos.X -= 1
 		}
 	}
 }
@@ -38,40 +59,40 @@ func (l *OrdinalWalkingStrategy) nextStep(walker *Walker) {
 	chosen_direction := rand.Intn(8)
 	switch chosen_direction {
 	case 0: // TOP
-		if walker.currentPos.Y < walker.height-1 {
-			walker.currentPos.Y += 1
+		if walker.CurrentPos.Y < walker.height-1 {
+			walker.CurrentPos.Y += 1
 		}
 	case 1: // TOP-RIGHT
-		if walker.currentPos.Y < walker.height-1 && walker.currentPos.X < walker.width-1 {
-			walker.currentPos.X += 1
-			walker.currentPos.Y += 1
+		if walker.CurrentPos.Y < walker.height-1 && walker.CurrentPos.X < walker.width-1 {
+			walker.CurrentPos.X += 1
+			walker.CurrentPos.Y += 1
 		}
 	case 2: // RIGHT
-		if walker.currentPos.X < walker.width-1 {
-			walker.currentPos.X += 1
+		if walker.CurrentPos.X < walker.width-1 {
+			walker.CurrentPos.X += 1
 		}
 	case 3: // BOTTOM-RIGHT
-		if walker.currentPos.X < walker.width-1 && walker.currentPos.Y > 0 {
-			walker.currentPos.X += 1
-			walker.currentPos.Y -= 1
+		if walker.CurrentPos.X < walker.width-1 && walker.CurrentPos.Y > 0 {
+			walker.CurrentPos.X += 1
+			walker.CurrentPos.Y -= 1
 		}
 	case 4: // BOTTOM
-		if walker.currentPos.Y > 0 {
-			walker.currentPos.Y -= 1
+		if walker.CurrentPos.Y > 0 {
+			walker.CurrentPos.Y -= 1
 		}
 	case 5: // BOTTOM-LEFT
-		if walker.currentPos.X > 0 && walker.currentPos.Y > 0 {
-			walker.currentPos.X -= 1
-			walker.currentPos.Y -= 1
+		if walker.CurrentPos.X > 0 && walker.CurrentPos.Y > 0 {
+			walker.CurrentPos.X -= 1
+			walker.CurrentPos.Y -= 1
 		}
 	case 6: // LEFT
-		if walker.currentPos.X > 0 {
-			walker.currentPos.X -= 1
+		if walker.CurrentPos.X > 0 {
+			walker.CurrentPos.X -= 1
 		}
 	case 7: // TOP-LEFT
-		if walker.currentPos.X > 0 && walker.currentPos.Y < walker.height-1 {
-			walker.currentPos.X -= 1
-			walker.currentPos.Y += 1
+		if walker.CurrentPos.X > 0 && walker.CurrentPos.Y < walker.height-1 {
+			walker.CurrentPos.X -= 1
+			walker.CurrentPos.Y += 1
 		}
 	}
 }
--- walker/walker.go
+++ walker/walker.go
@@ -1,23 +1,22 @@
 package walker
 
 import (
-	"math/rand"
-
 	"github.com/gopxl/pixel/v2"
 	"github.com/gopxl/pixel/v2/ext/imdraw"
 )
 
 type Walker struct {
-	currentPos      pixel.Vec
+	CurrentPos      pixel.Vec
 	width           float64
 	height          float64
 	walkingStrategy WalkingStrategy
 }
 
 func NewWalker(width, height float64, walkingStrtegy WalkingStrategy) *Walker {
-	startingPosition := pixel.V(rand.Float64()*width, rand.Float64()*height)
+	// startingPosition := pixel.V(rand.Float64()*width, rand.Float64()*height)
+	startingPosition := pixel.V(width/2, height/2)
 	walker := Walker{
-		currentPos:      startingPosition,
+		CurrentPos:      startingPosition,
 		width:           width,
 		height:          height,
 		walkingStrategy: walkingStrtegy,
@@ -28,7 +27,8 @@ func NewWalker(width, height float64, walkingStrtegy WalkingStrategy) *Walker {
 func (walker *Walker) NextStep() {
 	walker.walkingStrategy.nextStep(walker)
 }
-func (walker Walker) Draw(imd *imdraw.IMDraw) {
-	imd.Push(walker.currentPos)
-	imd.Circle(1, 0)
+func (walker Walker) Draw(imd *imdraw.IMDraw, prevPos pixel.Vec) {
+	imd.Push(walker.CurrentPos, prevPos)
+	imd.Line(2)
+	// imd.Circle(1, 0)
 }
